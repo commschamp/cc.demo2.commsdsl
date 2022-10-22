ac0d1039fd33e03f4b9c75ac84a80d8ffa9807a9
Alex Robenko <arobenko@gmail.com>
2022-10-22 16:40:10 +1000

    Added support for "swig" progject generation and build.

--- script/full_debug_build.sh
+++ script/full_debug_build.sh
@@ -10,6 +10,7 @@ ROOT_DIR=$( dirname ${SCRIPT_DIR} )
 export BUILD_DIR="${ROOT_DIR}/build.full.${CC}"
 export COMMON_INSTALL_DIR=${BUILD_DIR}/install
 export COMMON_BUILD_TYPE=Debug
+export EXTERNALS_DIR=${ROOT_DIR}/externals
 mkdir -p ${BUILD_DIR}
 
 ${SCRIPT_DIR}/prepare_externals.sh
@@ -17,11 +18,11 @@ ${SCRIPT_DIR}/prepare_externals.sh
 cd ${BUILD_DIR}
 cmake .. -DCMAKE_INSTALL_PREFIX=${COMMON_INSTALL_DIR} \
     -DCMAKE_BUILD_TYPE=Debug -DDEMO2_BUILD_GEN_TEST=ON \
-    -DDEMO2_BUILD_GEN_TOOLS=ON "$@"
+    -DDEMO2_BUILD_GEN_TOOLS=ON -DDEMO2_BUILD_GEN_SWIG=ON "$@"
 
 procs=$(nproc)
 if [ -n "${procs}" ]; then
     procs_param="--parallel ${procs}"
 fi
 
-cmake --build ${BUILD_DIR} --config ${COMMON_BUILD_TYPE} --target install
+cmake --build ${BUILD_DIR} --config ${COMMON_BUILD_TYPE} --target install ${procs_param}
